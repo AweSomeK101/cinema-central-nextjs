fbe445d94e8c3d4174458331d37d62520019e307
Samad K <samad.matrix963@gmail.com>
2024-12-15 22:11:07 +0530

    init and header

---
+++ src/components/root-header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="shadow-lg border-b border-gray-700 flex justify-center items-center min-h-16">
+      <h2 className="text-white uppercase text-center text-xl tracking-widest">
+        Cinema <span className="text-accent">Central</span>
+      </h2>
+    </header>
+  );
+}
+
+export default Header;
